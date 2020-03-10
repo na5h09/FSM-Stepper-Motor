378eecf27d01e3414f68ddc06264b6dd12aa52ce
na5h09 <42976853+na5h09@users.noreply.github.com>
2020-03-10 01:43:12 -0500

    Add files via upload

---
+++ StepperMotorController.c
@@ -0,0 +1,44 @@
+// StepperMotorController.c starter file EE319K Lab 5
+// Runs on TM4C123
+// Finite state machine to operate a stepper motor.  
+// Jonathan Valvano
+// 1/17/2020
+
+// Hardware connections (External: two input buttons and four outputs to stepper motor)
+//  PA5 is Wash input  (1 means pressed, 0 means not pressed)
+//  PA4 is Wiper input  (1 means pressed, 0 means not pressed)
+//  PE5 is Water pump output (toggle means washing)
+//  PE4-0 are stepper motor outputs 
+//  PF1 PF2 or PF3 control the LED on Launchpad used as a heartbeat
+//  PB6 is LED output (1 activates external LED on protoboard)
+
+#include "SysTick.h"
+#include "TExaS.h"
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+
+void EnableInterrupts(void);
+// edit the following only if you need to move pins from PA4, PE3-0      
+// logic analyzer on the real board
+#define PA4       (*((volatile unsigned long *)0x40004040))
+#define PE50      (*((volatile unsigned long *)0x400240FC))
+void SendDataToLogicAnalyzer(void){
+  UART0_DR_R = 0x80|(PA4<<2)|PE50;
+}
+
+int main(void){ 
+  TExaS_Init(&SendDataToLogicAnalyzer);    // activate logic analyzer and set system clock to 80 MHz
+  SysTick_Init();   
+// you initialize your system here
+	SYSCTL
+	
+  EnableInterrupts();   
+  while(1){
+// output
+// wait
+// input
+// next		
+  }
+}
+
+
